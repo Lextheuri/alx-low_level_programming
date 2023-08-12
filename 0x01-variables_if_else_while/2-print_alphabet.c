917a5a6c4b7ae08945937f9f6fcc6b39bddb9c65
Alexander Theuri <lextheuri90@gmail.com>
2023-08-12 09:32:23 -0700

    2-print_alphabet.c commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 int i;
-char n[26];
-for (i = 0; i < 26; i++)
+char n[27];
+for (i = 0; i < 27; i++)
 {
 n[i] = 'a' + i;
 }
-for (i = 0; i < 26; i++)
+for (i = 0; i < 27; i++)
 {
 putchar(n[i]);
 }
