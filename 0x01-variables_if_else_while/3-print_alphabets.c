ed0d4f53d492bc2bc5bfae13c4cbcd49c14c7c90
Alexander Theuri <lextheuri90@gmail.com>
2023-08-12 11:39:51 -0700

    3-print_alphabets.c commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
- * Description: print alphabet in lowercase then uppercase
+ * Description: Print alphabet in lowercase then uppercase
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i, j;
 char n[26], x[26];
-for (i = 0, j = 0; i < 26, j < 26; i++, j++)
+for (i = 0; i < 26; i++)
 {
 n[i] = 'a' + i;
-x[j] = 'A' + j;
+x[i] = 'A' + i;
 }
-for (i = 0, j = 0; i < 26, j < 26; i++, j++)
+for (i = 0; i < 26; i++)
 {
 putchar(n[i]);
-putchar('\n');
 }
 for (j = 0; j < 26; j++)
 {
 putchar(x[j]);
-putchar('\n');
 }
+putchar('\n');
 return (0);
 }
