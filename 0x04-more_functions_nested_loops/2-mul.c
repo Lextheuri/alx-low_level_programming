9c3ae508b2a4892453562e8eb3df92d5be5ff04a
Alexander Theuri <lextheuri90@gmail.com>
2023-08-18 10:24:13 -0700

    2-mul.c commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * * mul - multiply 2 int
+ * @a: first integer
+ * @b: second integer
+ * Return: results of the multiplicattion
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
