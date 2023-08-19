3c54716478376cd5673b0ec7e0c00602508d3f79
Alexander Theuri <lextheuri90@gmail.com>
2023-08-19 01:31:58 -0700

    5-more_numbers.c commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints x10 the numbers, from 0-14, followed by a new line
+ * Return (0) success
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+int n;
+for (n = 0; n <= 14; n++)
+{
+_putchar(n);
+}
+{
+_putchar('\n');
+}
+_putchar(i);
+}
+}
