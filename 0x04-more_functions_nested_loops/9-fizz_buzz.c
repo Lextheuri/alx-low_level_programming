1e846e905ae71c15355e4e51400e1127836bbd54
Alexander Theuri <lextheuri90@gmail.com>
2023-08-19 09:18:14 -0700

    9-fizz_buzz.c commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program to print multiples of 3 Fizz, 5 Buzz and both FizzBuzz
+ * Return: (0) success
+ */
+
+int main(void)
+{
+int i;
+for ( i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
+}
