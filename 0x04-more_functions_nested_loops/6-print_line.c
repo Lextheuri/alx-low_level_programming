e6d5a39ad647a743d3e726606beb2642671a568c
Alexander Theuri <lextheuri90@gmail.com>
2023-08-19 04:44:10 -0700

    6-print_line.c commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_line - Function that prints line in terminal
+ *@n: is the number of times _ to be printed
+ *
+ *Return: n if n is <= zero
+ */
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
