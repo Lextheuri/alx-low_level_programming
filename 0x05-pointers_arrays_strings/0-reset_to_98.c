d1afd25f63b520c8b67089f05fa9270bc01bbfdb
Alexander Theuri <lextheuri90@gmail.com>
2023-08-21 08:31:00 -0700

    0-reset_to_98.c commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 -function that takes a pointer to an int
+ * @n: as parameter and updates the value it points to to 98
+ *  return (98)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
