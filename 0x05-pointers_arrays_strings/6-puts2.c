eb466c92b2fc776cc45603bd14172323b997fc8d
Alexander Theuri <lextheuri90@gmail.com>
2023-08-22 10:33:27 -0700

    6-puts2.c commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 -function prints character of a string, from 1st,and new line.
+ * @str: pointer to string
+ * return: character of the string
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
