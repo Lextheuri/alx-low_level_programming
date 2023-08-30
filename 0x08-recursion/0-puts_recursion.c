79c313afc3e1ce80a2307866d7ace7bf848683f7
Alexander Theuri <lextheuri90@gmail.com>
2023-08-30 09:50:29 -0700

    0-puts_recursion.c commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string followed by a new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+while (*s != '\0')
+{
+_putchar(*s);
+*s++;
+}
+_putchar('\n');
+}
