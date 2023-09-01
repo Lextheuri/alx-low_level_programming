c43f12718c2a66d5eb6f76b68b4d5123e197b411
Alexander Theuri <lextheuri90@gmail.com>
2023-09-01 08:46:34 -0700

    10-add.c commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add -function that adds two integers and returns the result
+ * @a: input value
+ * @b: input value
+ * Return: results
+ */
+int add(int a, int b)
+{
+int result = a + b;
+return (result);
+}
