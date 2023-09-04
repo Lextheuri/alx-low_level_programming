8cc0a32451e3eb45cc6ce225655890a42638f1c6
Alexander Theuri <lextheuri90@gmail.com>
2023-09-04 03:27:47 -0700

    0x0A-argc_argv commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * whatsmyname - a program that prints its name, followed by a new line
+ * Return: (0)
+ */
+int main(int argc, char * argv[])
+{
+    printf("%s\n", argv[0]);
+
+    return 0;
+}
