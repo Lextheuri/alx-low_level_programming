b503046a3c5f7e1938afcec7f43c79cb9743f8b8
Alexander Theuri <lextheuri90@gmail.com>
2023-09-11 08:57:43 -0700

    4-free_grid.c commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - function that frees a 2 dimensional grid previously created
+ * @grid: function
+ * @height: array of integers
+ * Return: NULL
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
