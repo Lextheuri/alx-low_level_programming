42528be2ca36ee6156c36ed2056c7333f6118d99
lextheuri <lextheuri90@gmail.com>
2023-10-26 07:52:40 -0700

    0x08-recursion commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,44 +1,39 @@
-#include <stdio.h>
+#include "main.h"
+int _sister_function(int num, int root);
 /**
- * _sqrt_recursion - function that returns natural square root of a number
- * @n: input value
- * Return: (0) success
+ * _sqrt_recursion - Function that returns the natural square root of a number
+ * @n: The input value for which to calculate the square root
+ * Return: Square root of number, or -1 if not found
  */
-int _sqrt_helper(int n, int guess);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 {
 return (-1);
 }
-else if (n == 0 || n == 1)
-{
-return (n);
-}
 else
 {
-return (_sqrt_helper(n, n / 2));
+return (_sister_function(n, 0));
 }
 }
 /**
- * _sqrt_helper - helper function
- * @n: input value
- * @guess: input value
- * Return:(0) success
+ * _sister_function - Helper function that calculates square root recursively
+ * @num: The number for which to calculate the square root
+ * @root: The current guess for the square root
+ * Return: Square root of number, or -1 if not found
  */
-int _sqrt_helper(int n, int guess)
+int _sister_function(int num, int root)
 {
-int guess_squared = guess * guess;
-if (guess_squared == n)
+if ((root * root) > num)
 {
-return (guess);
+return (-1);
 }
-else if (guess_squared < n)
+else if ((root * root) == num)
 {
-return (_sqrt_helper(n, guess + 1));
+return (root);
 }
 else
 {
-return (_sqrt_helper(n, guess - 1));
+return (_sister_function(num, root + 1));
 }
 }
