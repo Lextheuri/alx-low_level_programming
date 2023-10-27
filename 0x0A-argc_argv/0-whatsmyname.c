e8c95cc0464a7f19e8b363c914c83781f96e86af
lextheuri <lextheuri90@gmail.com>
2023-10-27 15:39:34 -0700

    0x0A-argc_argv commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", __FILE__);
+printf("%s\n", *argv);
 return (0);
 }
