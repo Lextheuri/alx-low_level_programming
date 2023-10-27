98b4ebc6c0a0699e5cafd9fb48a7f5d26cbfe49b
lextheuri <lextheuri90@gmail.com>
2023-10-27 14:41:13 -0700

    0x09-static_libraries commit

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer to the & of string
+ * Return:length of string
+ */
+int _strlen(char *s)
+{
+  int length = 0;
+  while (*s != '\0')
+    {
+      length++;
+      s++;
+    }
+  return (length);
+}
